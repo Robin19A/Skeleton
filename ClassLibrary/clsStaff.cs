c24919b583064f7f2ebb22b5bfb5bf0c22f795e4
Robin19A <amanullahrobin19@gmail.com>
2024-05-21 11:20:31 +0100

    7

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ContactNumber { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
     }
 }
