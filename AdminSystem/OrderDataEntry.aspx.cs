1a65356fbf9406b58dfbf0343a6f6d3c3da73323
Munashe Musiwa <P2750963@my365.dmu.ac.uk>
2024-05-28 03:11:32 +0100

    end of prac 8

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //get the data from the session object
+        AnOrder = (clsOrder)Session["AnOrder"];
+        //display the house number for this entry
+        Response.Write(AnOrder.ShippingAddress);
     }
 
     protected void txtOrderId_TextChanged(object sender, EventArgs e)
@@ -19,6 +25,25 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //capture the Order Id
+        AnOrder.OrderId = Convert.ToInt16(txtOrderId.Text);
+        //capture the Customer Id
+        AnOrder.CustomerId = Convert.ToInt16(txtCustomerId.Text);
+        //capture the Order Date
+        AnOrder.OrderDate = Convert.ToDateTime(DateTime.Now);
+        //capture the Delivered Status
+        AnOrder.DeliveredStatus = txtDeliveredStatus.Text;
+        //capture the Total Amount
+        AnOrder.TotalAmount = Decimal.Parse(txtTotalAmount.Text);
+        //capture the Shipping Address
+        AnOrder.ShippingAddress = txtShippingAddress.Text;
+        //capture the Staff Id
+        AnOrder.StaffId = Convert.ToInt16(txtStaffID.Text);
+        //capture Complete check box
+        //store the address in the session object
+        Session["AnOrder"] = AnOrder;
         //navigate to the view page
         Response.Redirect("OrderViewer.aspx");
     }
