0f28ee2f0c061724d1a9a54525b12ddc02ef3e71
Robin19A <amanullahrobin19@gmail.com>
2024-06-04 21:19:41 +0100

    Week 4 Total Done

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //if this is the first time the page is displayed
+        if(IsPostBack == false)
+        {
+            DisplayStaffes();
+        }
+    }
+    void DisplayStaffes()
+    {
+        clsStaffCollection Staffes = new clsStaffCollection();
+        lstStaffList.DataSource = Staffes.StaffList;
+        lstStaffList.DataValueField = "StaffId";
+        lstStaffList.DataTextField = "Address";
+        lstStaffList.DataBind();
     }
 }
