d2595e2b287e51d7e3ebca96c34cea2d0ada78db
Robin19A <amanullahrobin19@gmail.com>
2024-06-06 21:21:53 +0100

    Lab 15 is done

---
+++ AdminSystem/StaffLogin.aspx.cs
@@ -0,0 +1,52 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class StaffLogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        //create an instance of the address user class
+        clsStaffUser AnUser = new clsStaffUser();
+        //create the variablesto store the username and password
+        string Username = txtUsername.Text;
+        string Password = txtPassword.Text;
+        //create a variable to store the result of the find user operation
+        Boolean Found = false;
+        //get the username entered by the user
+        Username = Convert.ToString(txtUsername.Text);
+        //get the password entered by the user
+        Password = Convert.ToString(txtPassword.Text);
+        //find the record
+        Found = AnUser.FindUser(Username, Password);
+        //if  username and/or password is empty
+        if (txtUsername.Text == "")
+        {
+            lblError.Text = "Enter a Username";
+        }
+        else if (txtPassword.Text == "")
+        {
+            lblError.Text = "Enter a Password";
+        }
+        //if found
+        else if (Found == true)
+        {
+            Response.Redirect("StaffList.aspx");
+        }
+        else if (Found == false)
+        {
+            lblError.Text = "Loging informations are incorrect. Please try again";
+        }
+
+
+    }
+}
---
+++ ClassLibrary/clsStaffUser.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+
+namespace ClassLibrary
+{
+    public class clsStaffUser
+    {
+        //private data member for the user id property
+        private Int32 mUserID;
+        //private data member for the user name property
+        private string mUsername;
+        //private data member for the user password property
+        private string mPassword;
+        ////private data member for the user depertment property
+        private string mDepartment;
+
+        public int UserID
+        {
+            get
+            {
+                //return the private data
+                return mUserID;
+            }
+            set
+            {
+                //set the private data
+                mUserID = value;
+            }
+        }
+        public string Username 
+        {
+            get
+            {
+                //return the private data
+                return mUsername;
+            }
+            set
+            {
+                //set the private data
+                mUsername = value;
+            }
+        }
+        public string Password 
+        {
+            get
+            {
+                //return the private data
+                return mPassword;
+            }
+            set
+            {
+                //set the private data
+                mPassword = value;
+            }
+        }
+        public string Department 
+        {
+            get
+            {
+                //return the private data
+                return mDepartment;
+            }
+            set
+            {
+                //set the private data
+                mDepartment = value;
+            }
+        }
+
+        public bool FindUser(string Username, string Password)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameters for the user username and password to search for
+            DB.AddParameter("@Username", Username);
+            DB.AddParameter("@Password", Password);
+            //execute the stored procedure
+            DB.Execute("sproc_tblUsers_FindUsernamePW");
+            //if one record is found (there should be either one or more)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mUserID = Convert.ToInt32(DB.DataTable.Rows[0]["UserID"]);
+                mUsername = Convert.ToString(DB.DataTable.Rows[0]["Username"]);
+                mPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                mDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+                //return true to confirm everything worked ok
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+        }
+    }
+}
